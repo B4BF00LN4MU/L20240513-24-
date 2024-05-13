6289d89946330efc9a3be0e761e690d3fdda0e87
B4BF00LN4MU <boy566@naver.com>
2024-05-13 10:26:55 +0900

    ddd

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A = 5;
+	int B = 3;
+
+	cout << "A = " << A << endl;
+	cout << "B = " << B << endl;
+	cout << "A + B = " << A + B << endl;
+	cout << "A - B = " << A - B << endl;
+	cout << "A * B = " << A * B << endl;
+	cout << "A / B = " << A / B << endl;
+	cout << "A % B = " << A % B << endl;
+
+	return 0;//jafjaf;jfdsa;lkjfdsalkj;dd
+}
